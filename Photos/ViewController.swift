fc4101c8033341f85f8a9a46e2d0f826a8c2db39
Vlad Filyakov <vladf@microsoft.com>
2020-01-08 12:15:55 -0800

    Initial

---
+++ Photos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photos
+//
+//  Created by Vladislav Filyakov on 1/8/20.
+//  Copyright © 2020 Vlad Filyakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
